5c08277c2cfd010fbb93cd5fbf284cfeb90234e5
HuydDo <huydinhdo@yahoo.com>
2020-04-09 11:49:02 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
+// write your CatList component here
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
+class App extends Component {
 
-class App extends Component {   
-  
-  render() {
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
+
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading){
+      return <div>Loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
+
+  render(){
+    console.log(this.props.catPics)
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.handleLoading()}
       </div>
-    );
+    )
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,14 @@
 // write and export your action creator function here
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'})
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+    .then(respond => {
+      return respond.json()})
+    .then(responseJSON => {
+      dispatch({ type: 'ADD_CATS', cats:
+      responseJSON.images    
+    })
+    })
+  }
+}
