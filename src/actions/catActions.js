cdd2b7882076523de99889303042decf1d222e26
Sam Markham <sam.e.markham@gmail.com>
2020-04-14 11:40:10 -0700

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,22 @@
 // write and export your action creator function here
+// export const fetchCats = () => {
+//   fetch()
+//   .then(res => res.json())
+//   .then(data => {
+//     return({
+//       type: 'ADD_CATS',
+//       payload: data
+//     })
+//   })
+// }
+
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_CATS" })
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+    .then(res => res.json())
+    .then(resJSON => {
+      dispatch({type: "ADD_CATS", cats: resJSON.images})
+    })
+  }
+}
